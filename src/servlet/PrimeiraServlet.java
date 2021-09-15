6b9305dcbb8deac2359f221dafbbf20558ad1faf
jair urbano <62015340+vieuxcartable@users.noreply.github.com>
2021-09-15 09:04:44 -0300

    Add files via upload
    Master

---
+++ src/servlet/PrimeiraServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/bemVindo")
+public class PrimeiraServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hello World!");
+		out.println("</body>");
+		out.println("</html>");
+
+		System.out.println("Bem vindo");
+
+	}
+}
